7cf04e23f3e7754be1058bbc265e5b73b14248b0
oscar <oscar.vasquez@zentro.com.mx>
2023-05-23 15:30:07 -0600

    168. Alineando contenido

---
+++ App.tsx
@@ -0,0 +1,60 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App = ()=> {
+  
+  return (
+   <>
+      <View style={styles.contenedor}>
+          <View style={styles.caja1}></View>
+          <View style={styles.caja2}></View>
+          <View style={styles.caja3}></View>
+          <View style={styles.caja4}></View> 
+      </View>
+   </>
+  );
+}
+
+const styles = StyleSheet.create({
+  contenedor:{
+    backgroundColor:'cornflowerblue',
+    flex: 1,
+    flexDirection: 'column',
+    // justifyContent: 'center',
+    // alignItems: 'center'
+  },
+  caja1:{
+    padding:20,
+    backgroundColor: 'navy',
+    // flex: 1
+  },
+  caja2:{
+    padding:20,
+    backgroundColor: 'yellow',
+    // flex: 1
+  },
+  caja3:{
+    padding:20,
+    backgroundColor: 'green',
+    // flex: 1
+  },
+  caja4:{
+    padding:20,
+    backgroundColor: 'teal',
+    // flex: 1
+  },
+})
+
+
+export default App;
